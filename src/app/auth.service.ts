112fc75ca5ad9269ef14f160eaeec42935d2ebb1
Leif Hetland <lehetland1@gmail.com>
2018-05-03 14:18:30 -0500

    implemented show hide for admin links based on if admin or not

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { ActivatedRoute } from '@angular/router'
+import { AppUser } from './models/app-user';
+import { UserService } from './user.service';
+import { ActivatedRoute } from '@angular/router';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/observable/of';
 
 @Injectable()
 export class AuthService {
   user$: Observable<firebase.User>;
 
-  constructor(private afAuth: AngularFireAuth, private route: ActivatedRoute) {
+  constructor(
+    private userService: UserService,
+    private afAuth: AngularFireAuth,
+    private route: ActivatedRoute) {
     this.user$ = afAuth.authState;
   }
 
@@ -21,5 +29,16 @@ export class AuthService {
 
   logout() {
     this.afAuth.auth.signOut();
+  }
+
+  get appUser$(): Observable<AppUser> {
+    return this.user$
+      .switchMap(user => {
+        if (user) {
+          return this.userService.get(user.uid);
+        } else {
+          return Observable.of(null);
+        }
+      })
   }
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLinkActive } from '@angular/router';
 import { AuthService } from './../auth.service';
+import { AppUser }  from './../models/app-user';
 
 @Component({
   selector: 'navbar',
@@ -8,9 +9,10 @@ import { AuthService } from './../auth.service';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  appUser: AppUser;
 
-  constructor(public auth: AuthService) {
-
+  constructor(private auth: AuthService) {
+    auth.appUser$.subscribe(appUser => this.appUser = appUser);
   }
 
   logout() {
