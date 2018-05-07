34a44f20ffd47e0837d37f380d9b232a47e0e915
Leif Hetland <lehetland1@gmail.com>
2018-05-07 17:15:16 -0500

    building out angular material form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,6 +29,7 @@ import { AuthService } from './auth.service';
 import { UserService } from './user.service';
 import { AuthGuard } from './auth-guard.service';
 import { AdminAuthGuard } from './admin-auth-guard.service';
+import { ProductFormComponent } from './admin/product-form/product-form.component';
 
 
 @NgModule({
@@ -43,7 +44,8 @@ import { AdminAuthGuard } from './admin-auth-guard.service';
     MyOrdersComponent,
     AdminProductsComponent,
     AdminOrdersComponent,
-    LoginComponent
+    LoginComponent,
+    ProductFormComponent
   ],
   imports: [
     BrowserModule,
@@ -70,6 +72,11 @@ import { AdminAuthGuard } from './admin-auth-guard.service';
         component: AdminProductsComponent,
         canActivate: [AuthGuard, AdminAuthGuard]
        },
+       {
+         path: 'admin/products/new',
+         component: ProductFormComponent,
+         canActivate: [AuthGuard, AdminAuthGuard]
+        },
       {
         path: 'admin/orders',
         component: AdminOrdersComponent,
