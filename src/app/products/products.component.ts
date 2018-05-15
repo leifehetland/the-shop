61f462df0fb6a87a7dc821e9aec1f50176fa6814
Leif Hetland <lehetland1@gmail.com>
2018-05-14 21:02:27 -0500

    using ng container to display bootstrap card of game products in two columns

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ProductService } from './../product.service';
 
 @Component({
   selector: 'products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent {
+  products$;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private productService: ProductService) {
+    this.products$ = productService.getAll();
   }
+
 
 }
