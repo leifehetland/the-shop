12f641e6dd0bcc271805fab7d4a75a2f1ae13910
Leif Hetland <lehetland1@gmail.com>
2018-05-14 22:26:40 -0500

    filtering games by category/genre

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { ProductService } from './../product.service';
 import { CategoryService } from './../category.service';
+import { Product } from './../models/product';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'products',
@@ -8,12 +11,35 @@ import { CategoryService } from './../category.service';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
-  products$;
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
   categories$;
+  category: string;
 
-  constructor(productService: ProductService, categoryService: CategoryService) {
-    this.products$ = productService.getAll();
+  constructor(
+    route: ActivatedRoute,
+    productService: ProductService,
+    categoryService: CategoryService) {
+    productService
+      .getAll()
+      .switchMap(products => {
+        // Get the products, then subscribe to query param map observable
+        this.products = products;
+        return route.queryParamMap;
+      })
+      .subscribe(params => {
+        this.category = params.get('category');
+
+        // Setting the filtered products array
+        this.filteredProducts = (this.category) ?
+          this.products.filter(p => p.category === this.category) :
+          this.products;
+      });
+
+    // Getting all the categories back from service
     this.categories$ = categoryService.getAll();
+
+
   }
 
 
