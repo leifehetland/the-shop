1e3767eaf221154d23037c15a451fafa2f5f5f16
Leif Hetland <lehetland1@gmail.com>
2018-05-15 15:36:44 -0500

    adding and subtracting quantity from shopping cart using buttons on product card

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -17,13 +17,17 @@ export class ProductCardComponent {
 
   constructor(private cartService: ShoppingCartService) {}
 
-  addToCart(product: Product) {
+  addToCart() {
     this.cartService.addToCart(this.product);
+  }
+
+  removeFromCart() {
+    this.cartService.removeFromCart(this.product);
   }
 
   getQuantity() {
     if (!this.shoppingCart) return 0;
-    
+
     let item = this.shoppingCart.items[this.product.key];
     return item ? item.quantity : 0;
   }
